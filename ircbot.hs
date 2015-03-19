da8f2b9852ff9e0f4e98b049f4a7bfa9de3890be
Dima Gorbik <dgorbik@apple.com>
2015-03-19 10:48:07 -0700

    Replace hsOpenSSL with TLS

--- ircbot.hs
+++ ircbot.hs
@@ -1,13 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Network.Socket hiding (listen, Connected)
-import OpenSSL
-import qualified OpenSSL.Session as SSL
+import Network.Connection
+  (Connection
+  , TLSSettings (TLSSettingsSimple)
+  , ConnectionParams (ConnectionParams, connectionHostname, connectionPort, connectionUseSecure, connectionUseSocks)
+  , initConnectionContext
+  , connectTo
+  , ConnectionParams
+  , connectionGetLine
+  , connectionPut
+  )
 import System.IO 
-import qualified GitLab as GitLab
+import qualified GitLab
 import Text.Printf
 import qualified Network.IRC as IRC
 import Web.Scotty
-import Network.HTTP.Types.Status
 
 import Control.Monad.STM
 import Control.Concurrent
@@ -22,67 +28,57 @@ import Control.Monad.State (StateT, evalStateT, lift)
 import qualified Control.Monad.State as S
 import Data.List (isPrefixOf, isInfixOf)
 import Data.Monoid (mconcat)
+import Data.Default (def)
 
 server = "irc.freenode.org"
 port   = 6697
 nick   = "CoolestBot123048"
 chan   = "#bottest"
-nick_password = "password123"
+nickPassword = "password123"
 
-connectToServer :: AddrInfo -> IO SSL.SSL
-connectToServer saddr = do
-  sServer <- socket (addrFamily saddr) Stream defaultProtocol
-  putStrLn "connecting"
-  connect sServer (addrAddress saddr)
-  putStrLn "establishing ssl context"
-  ctx <- SSL.context
-  putStrLn "setting ciphers"
-  SSL.contextSetCiphers ctx "DEFAULT"
-  putStrLn "setting verfication mode"
-  SSL.contextSetVerificationMode ctx SSL.VerifyNone
-  putStrLn "making ssl connection"
-  sslServer <- SSL.connection ctx sServer
-  putStrLn "doing handshake"
-  SSL.connect sslServer
-  putStrLn "connected"
-  write sslServer ("NICK " ++ nick)
-  write sslServer ("USER " ++ nick ++ " 0 * :test bot")
-  return sslServer
+connectToServer ::  IO Connection
+connectToServer = do
+  ctx <- initConnectionContext
+  con <- connectTo ctx ConnectionParams
+                  { connectionHostname = server
+                  , connectionPort = port                       
+                  , connectionUseSecure = Just $ TLSSettingsSimple True False False
+                  , connectionUseSocks = Nothing
+                  }                       
+  write con ("NICK " ++ nick)
+  write con ("USER " ++ nick ++ " 0 * :test bot")
+  return con
 
-listen :: SSL.SSL -> IO ()
-listen ssl = do
-  lines <- BL.lines `fmap` SSL.lazyRead ssl
-  evalStateT (mapM_ (processLine ssl) lines) Initial
+listen :: Connection -> IO ()
+listen ssl = evalStateT (forever (liftIO (connectionGetLine 512 ssl) >>= processLine ssl)) Initial >> return ()
 
 data ConnectionState = Initial | NickIdentification | Connected
 
-processQueue :: SSL.SSL -> TQueue String -> IO ()
+processQueue :: Connection -> TQueue String -> IO ()
 processQueue ssl queue = forever $ do newData <- atomically $ readTQueue queue
                                       privmsg ssl chan newData
 
-processLine :: SSL.SSL -> BL.ByteString -> StateT ConnectionState IO ()
+processLine :: Connection -> B.ByteString -> StateT ConnectionState IO ()
 processLine ssl line = case message of
                          Just a -> processMessage ssl a
                          Nothing -> return ()
-  where message = IRC.decode $ BL.toStrict line
+  where message = IRC.decode line
 
-processMessage :: SSL.SSL -> IRC.Message -> StateT ConnectionState IO ()
+processMessage :: Connection -> IRC.Message -> StateT ConnectionState IO ()
 processMessage ssl m = do
     state <- S.get
     case state of
       Initial -> when (IRC.msg_command m == "376") $ do
         liftIO $ do putStrLn "connected!"
-                    privmsg ssl "NickServ" ("identify " ++ nick_password)
+                    privmsg ssl "NickServ" ("identify " ++ nickPassword)
         S.put NickIdentification
-      NickIdentification -> do
+      NickIdentification ->
         when (identified m) $ do
           liftIO $ do putStrLn "identified!"
                       joinChannel ssl chan
           S.put Connected
       Connected -> return ()
     liftIO $ print m
-    -- let s = (tail . init . show) line
-    -- putStrLn s
     when (IRC.msg_command m == "PING") $
       (liftIO . pong . mconcat . map show) (IRC.msg_params m)
   where
@@ -91,15 +87,15 @@ processMessage ssl m = do
     isNickServ _ = False
     identified m = isNickServ (IRC.msg_prefix m) && "now identified" `isInfixOf` (mconcat . map show) (IRC.msg_params m)
 
-privmsg :: SSL.SSL -> String -> String -> IO ()
+privmsg :: Connection -> String -> String -> IO ()
 privmsg h dest s = write h ("PRIVMSG " ++ dest ++ " :" ++ s)
 
-joinChannel :: SSL.SSL -> String -> IO ()
+joinChannel :: Connection -> String -> IO ()
 joinChannel s c = write s ("JOIN " ++ c)
            
-write :: SSL.SSL -> String -> IO ()
+write :: Connection -> String -> IO ()
 write h s = do
-    SSL.write h $ B.pack (s ++ "\r\n")
+    connectionPut h $ B.pack (s ++ "\r\n")
     printf    "> %s\n" s
 
 -- web_service :: TQueue String -> IO ()
@@ -108,14 +104,11 @@ write h s = do
 --     liftIO $ atomically $ writeTQueue queue "test passed"    
 --     status ok200
 
-runIrcBot :: [AddrInfo] -> TQueue String -> IO ()
-runIrcBot addrs queue = do ssl <- connectToServer (head addrs)
-                           forkIO $ listen ssl
-                           processQueue ssl queue
+runIrcBot :: TQueue String -> IO ()
+runIrcBot queue = do ssl <- connectToServer
+                     forkIO $ listen ssl
+                     processQueue ssl queue
 
-main = withOpenSSL $ do
-  let hints = defaultHints { addrSocketType = Stream, addrFamily = AF_INET }
-  addrs <- getAddrInfo (Just hints) (Just "irc.freenode.com") (Just "6697")
-  queue <- atomically newTQueue
-  forkIO $ runIrcBot addrs queue
+main =  do queue <- atomically newTQueue
+           forkIO $ runIrcBot queue
   -- web_service queue
