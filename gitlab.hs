a45b5aaf8259cf6b94cd9271ae2b4f6d4c5a3e45
Dima Gorbik <dgorbik@apple.com>
2015-03-19 10:14:25 -0700

    Nick identification and gitlab module

---
+++ gitlab.hs
@@ -0,0 +1,36 @@
+{-# LANGUAGE OverloadedStrings #-}
+module GitLab where
+       
+import Data.Aeson
+import Control.Applicative
+import Data.Monoid
+import qualified Data.ByteString.Lazy.Char8 as BL
+
+data Commit = Commit { id :: String
+                     , message :: String
+                     , author :: String
+                     , email :: String  
+                     , url :: String  
+                     } deriving (Eq)
+
+instance Show Commit where
+  show x = mconcat [author x, " (", email x, ") committed: ", message x]
+
+instance FromJSON Commit where
+  parseJSON (Object o) = Commit <$>
+                         o .: "id" <*>
+                         o .: "message" <*>
+                         ((o .: "author") >>= (.: "name")) <*>
+                         ((o .: "author") >>= (.: "email")) <*>
+                         o .: "url"
+                         
+data CommitHook = CommitHook { ref :: String
+                             , projectId :: Int
+                             , commits :: [Commit]
+                             } deriving (Show, Eq) 
+
+instance FromJSON CommitHook where
+  parseJSON (Object o) = CommitHook <$>
+                         o .: "ref" <*>
+                         o .: "project_id" <*>
+                         (o .: "commits" >>= parseJSON)
--- ircbot.hs
+++ ircbot.hs
@@ -1,10 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
-import Network.Socket hiding (listen)
+import Network.Socket hiding (listen, Connected)
 import OpenSSL
 import qualified OpenSSL.Session as SSL
 import System.IO 
+import qualified GitLab as GitLab
 import Text.Printf
-  
+import qualified Network.IRC as IRC
 import Web.Scotty
 import Network.HTTP.Types.Status
 
@@ -17,12 +18,16 @@ import qualified Data.ByteString.Char8 as B
 
 import Control.Monad (when, forever)
 import Control.Monad.Trans (liftIO)
-import Data.List (isPrefixOf)
+import Control.Monad.State (StateT, evalStateT, lift)
+import qualified Control.Monad.State as S
+import Data.List (isPrefixOf, isInfixOf)
+import Data.Monoid (mconcat)
 
 server = "irc.freenode.org"
-port   = 6667
+port   = 6697
 nick   = "CoolestBot123048"
 chan   = "#bottest"
+nick_password = "password123"
 
 connectToServer :: AddrInfo -> IO SSL.SSL
 connectToServer saddr = do
@@ -42,49 +47,75 @@ connectToServer saddr = do
   putStrLn "connected"
   write sslServer ("NICK " ++ nick)
   write sslServer ("USER " ++ nick ++ " 0 * :test bot")
-  write sslServer ("JOIN " ++ chan)
   return sslServer
 
 listen :: SSL.SSL -> IO ()
 listen ssl = do
   lines <- BL.lines `fmap` SSL.lazyRead ssl
-  mapM_ (processLine ssl . show) lines
+  evalStateT (mapM_ (processLine ssl) lines) Initial
+
+data ConnectionState = Initial | NickIdentification | Connected
 
 processQueue :: SSL.SSL -> TQueue String -> IO ()
 processQueue ssl queue = forever $ do newData <- atomically $ readTQueue queue
-                                      privmsg ssl newData
+                                      privmsg ssl chan newData
 
-processLine :: SSL.SSL -> String -> IO ()
-processLine ssl line = do
-    let s = (tail . init) line
-    putStrLn s
-    when (ping s) $ pong s 
+processLine :: SSL.SSL -> BL.ByteString -> StateT ConnectionState IO ()
+processLine ssl line = case message of
+                         Just a -> processMessage ssl a
+                         Nothing -> return ()
+  where message = IRC.decode $ BL.toStrict line
+
+processMessage :: SSL.SSL -> IRC.Message -> StateT ConnectionState IO ()
+processMessage ssl m = do
+    state <- S.get
+    case state of
+      Initial -> when (IRC.msg_command m == "376") $ do
+        liftIO $ do putStrLn "connected!"
+                    privmsg ssl "NickServ" ("identify " ++ nick_password)
+        S.put NickIdentification
+      NickIdentification -> do
+        when (identified m) $ do
+          liftIO $ do putStrLn "identified!"
+                      joinChannel ssl chan
+          S.put Connected
+      Connected -> return ()
+    liftIO $ print m
+    -- let s = (tail . init . show) line
+    -- putStrLn s
+    when (IRC.msg_command m == "PING") $
+      (liftIO . pong . mconcat . map show) (IRC.msg_params m)
   where
-    ping x = "PING :" `isPrefixOf` x
-    pong x = write ssl ("PONG :" ++ drop 6 x) 
+    pong x = write ssl ("PONG :" ++ x) 
+    isNickServ (Just (IRC.NickName "NickServ" _ _)) = True
+    isNickServ _ = False
+    identified m = isNickServ (IRC.msg_prefix m) && "now identified" `isInfixOf` (mconcat . map show) (IRC.msg_params m)
 
-privmsg :: SSL.SSL -> String -> IO ()
-privmsg h s = write h ("PRIVMSG " ++ chan ++ " :" ++ s)
+privmsg :: SSL.SSL -> String -> String -> IO ()
+privmsg h dest s = write h ("PRIVMSG " ++ dest ++ " :" ++ s)
 
+joinChannel :: SSL.SSL -> String -> IO ()
+joinChannel s c = write s ("JOIN " ++ c)
+           
 write :: SSL.SSL -> String -> IO ()
 write h s = do
     SSL.write h $ B.pack (s ++ "\r\n")
     printf    "> %s\n" s
 
-web_service :: TQueue String -> IO ()
-web_service queue = scotty 25000 $ do
-  get "/" $ do
-    liftIO $ atomically $ writeTQueue queue "test passed"    
-    status ok200
+-- web_service :: TQueue String -> IO ()
+-- web_service queue = scotty 25000 $ do
+--   get "/" $ do
+--     liftIO $ atomically $ writeTQueue queue "test passed"    
+--     status ok200
 
-irc_bot :: [AddrInfo] -> TQueue String -> IO ()
-irc_bot addrs queue = do ssl <- connectToServer (head addrs)
-                         forkIO $ listen ssl
-                         processQueue ssl queue
+runIrcBot :: [AddrInfo] -> TQueue String -> IO ()
+runIrcBot addrs queue = do ssl <- connectToServer (head addrs)
+                           forkIO $ listen ssl
+                           processQueue ssl queue
 
 main = withOpenSSL $ do
   let hints = defaultHints { addrSocketType = Stream, addrFamily = AF_INET }
   addrs <- getAddrInfo (Just hints) (Just "irc.freenode.com") (Just "6697")
   queue <- atomically newTQueue
-  forkIO $ irc_bot addrs queue
-  web_service queue
+  forkIO $ runIrcBot addrs queue
+  -- web_service queue
